f6e7530250e4189253cd8088499c3291ca6d95da
faithege <faithege@hotmail.co.uk>
2019-04-15 20:28:13 +0100

    Update readAll.php

--- views/blogs/readAll.php
+++ views/blogs/readAll.php
@@ -4,11 +4,11 @@
 <?php foreach($blogs as $blog) { ?>
   <p>
     <?php echo $blog->title ."<br>";
-    echo $blog->content;
-    echo $blog->countryName;
-    echo $blog->continentName;
-    echo $blog->userID;
-    echo $blog->categoryName;
+    
+    echo $blog->countryName."<br>";
+    echo $blog->continentName."<br>";
+    echo $blog->username."<br>";
+    echo $blog->categoryName."<br>";
     ?> &nbsp; &nbsp;
 <!--    <a href='?controller=product&action=read&blogID= echo $blog->blogID; '> test </a> &nbsp; &nbsp;
     <a href='?controller=product&action=delete&blogID= echo $blog->blogID; '> </a> &nbsp; &nbsp;
