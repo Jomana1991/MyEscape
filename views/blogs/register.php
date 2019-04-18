655f19c08df3ec62d42d8db4f15d17cc000f434d
Jomana <alahmad.jomana@gmail.com>
2019-04-18 18:51:54 +0100

    added user and register

---
+++ views/blogs/register.php
@@ -0,0 +1,8 @@
+<p>Please register below</p>
+<form method='POST' action=" ">
+    <input type="text" name="Username" placeholder="Username" value="" >
+    <input type="password" name="Password" placeholder="Password" value="" >
+    <input type="email" name="Email" placeholder="Email" value="" >
+    <input type="submit" value="Go to login" />
+</form>
+<?php
