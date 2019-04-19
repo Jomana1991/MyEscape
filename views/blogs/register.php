32388ddfa3fe518399208f35688026523a7beca9
faithege <faithege@hotmail.co.uk>
2019-04-19 18:33:31 +0100

    Delete register.php

--- views/blogs/register.php
+++
@@ -1,8 +0,0 @@
-<p>Please register below</p>
-<form method='POST' action=" ">
-    <input type="text" name="Username" placeholder="Username" value="" >
-    <input type="password" name="Password" placeholder="Password" value="" >
-    <input type="email" name="Email" placeholder="Email" value="" >
-    <input type="submit" value="Go to login" />
-</form>
-<?php
