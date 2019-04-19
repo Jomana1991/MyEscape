a5ca59c13b177ea9a14dda0374382fec9ad27ddc
faithege <faithege@hotmail.co.uk>
2019-04-20 00:01:06 +0100

    Update search.php

--- views/blogs/search.php
+++ views/blogs/search.php
@@ -19,32 +19,37 @@ session_start();
 
 
 
-<p>results</p>
+<h4>Results</h4>
 
 <?php 
-if (isset($_POST['query'])) {
+    if (isset($_POST['query'])) {
 
- foreach ($blogs as $blog) { 
-    
-       
-        echo $blog->title . "<br>";
+        foreach ($blogs as $blog) { 
 
-        echo $blog->countryName . "<br>";
-        echo $blog->continentName . "<br>";
-        echo $blog->username . "<br>";
-        echo $blog->categoryName . "<br>";
 
-        //If blog is greater than 200 characters, the content will be shortened to 150 characters, if not the whole content will be echo'd
-        if (strlen($blog->content) > 150) {
-            echo substr($blog->content, 0, strpos(wordwrap($blog->content, 150), "\n")) . '...' . "<br>";
-//        here I am using wordwrap to line breakat the nearest word to 150 characters(so you don't get half words),
-//        strpos then returns the position of the first line break, and the content will therefore be shortened to this position by substr
-        } else {
-            echo $blog->content . "<br>";
-        }
-        ?> &nbsp; &nbsp;
+            echo "<strong>".$blog->title ."</strong>". "<br>";
+            echo $blog->username . "<br>";
+            echo $blog->categoryName . "<br>";
+            echo $blog->countryName . "<br>";
+            echo $blog->continentName . "<br>";
 
-<?php } } else { echo 'please search for blog'; } 
-?>
+
+            //If blog is greater than 200 characters, the content will be shortened to 150 characters, if not the whole content will be echo'd
+            if (strlen($blog->content) > 150) {
+                echo substr($blog->content, 0, strpos(wordwrap($blog->content, 150), "\n")) . '...' . "<br><br>";
+    //        here I am using wordwrap to line breakat the nearest word to 150 characters(so you don't get half words),
+    //        strpos then returns the position of the first line break, and the content will therefore be shortened to this position by substr
+            } else {
+                echo $blog->content . "<br><br>";
+            }
+
+            }
+    }
+    else { 
+        echo 'No results, please try searching for something else';
+    }
+        ?> 
+&nbsp; &nbsp;
+
 
 
