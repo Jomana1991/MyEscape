8dc37b82275fe7790f73b50e9099b4c8b5f66d9b
faithege <faithege@hotmail.co.uk>
2019-04-20 11:38:36 +0100

    Revert "Merge branch 'master' of https://github.com/Jomana1991/MyEscape"
    This reverts commit 4a7d02101d8bfea8e20d248dc1048bc498e17612, reversing
    changes made to a48a04bfb4832e17c789c5e3dc013f2382875627.

--- views/users/register.php
+++ views/users/register.php
@@ -1,31 +1,8 @@
 <p>Please register below</p>
-
-<!--<script src="pwdConfirm.js" type="text/javascript"></script> -->
-
-<form method='POST' id="RegForm" name="RegForm" action="" onsubmit="return checkPassword()">
-    <input type="text" id="Username" name="Username" placeholder="Username" value="" tabindex="1" required >
-    <input type="email" id="Email" name="Email" placeholder="Email" value="" tabindex="2" required >
-    <input type="password" id="Password" name="Password" placeholder="Password" value="" tabindex="3" required >
-    <input type="password" id="ConfirmPassword" name="ConfirmPassword" id="password_confirmation" placeholder="Confirm Password" tabindex="4" required>
-    <input type="submit" value="Go to login" tabindex="5"/>
-    <a href="login.php"> Already registered? <br> Click here to Login</a>
-    <!-- Comment-->
+<form method='POST' action=" ">
+    <input type="text" name="Username" placeholder="Username" value="" >
+    <input type="password" name="Password" placeholder="Password" value="" >
+    <input type="email" name="Email" placeholder="Email" value="" >
+    <input type="submit" value="Go to login" />
 </form>
-<script>
-function checkPassword()
-{
-    $password = document.forms["RegForm"]["Password"].value;     // to understand this syntax pls refer JS forms in W3schools website.
-    $confirmation = document.forms["RegForm"]["ConfirmPassword"].value;
-    if($password !== $confirmation)
-    {
-        alert("Passwords do not match"); 
-        return false;
-    }
-    else
-    {
-        return true;
-    }
-     
-}
-
-</script>
+<?php
