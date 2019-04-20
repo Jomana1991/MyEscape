d5ae3b26eaf5953dea20b4a71981ee9e905140be
faithege <faithege@hotmail.co.uk>
2019-04-21 00:33:24 +0100

    comment out session start
    now in layout

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,7 +1,7 @@
 <H1>My Escape </H1>
 <p>Explore. Travel. Share</p>
 <?php 
-    session_start();
+    #session_start();//removed as now in layout
     if (!empty($_SESSION['username'])){
         echo '<p>Hi '.$_SESSION['username'].' , you are still logged in - want to visit your '
                 . "<a href='?controller=blog&action=create'>profile page </a>?</p>";
