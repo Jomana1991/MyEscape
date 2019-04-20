607fce8ef19a604a7224930945237158a98c85b7
faithege <faithege@hotmail.co.uk>
2019-04-20 11:40:39 +0100

    Update login.php

--- views/users/login.php
+++ views/users/login.php
@@ -1,8 +1,8 @@
 
 <p>please login below</p>
 <form method='POST' action=" ">
-<input type="text" name="username" value="" >
-<input type="password" name="password" value="" >
+<input type="text" name="username" value="" required>
+<input type="password" name="password" value="" required>
 
     <input type="submit" value="Go to profile" />
 </form>
