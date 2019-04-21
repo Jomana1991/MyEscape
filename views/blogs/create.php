329433d25a27834fdd8d88c4563c5c53dabe182d
vmashwini <ashwini.mandar@gmail.com>
2019-04-21 22:42:17 +0100

    Update create.php

--- views/blogs/create.php
+++ views/blogs/create.php
@@ -26,7 +26,8 @@ $result_cou = $stmt_cou->fetchAll(PDO::FETCH_ASSOC);
     <head>
         <meta charset="UTF-8">
         <title>Add New blog post</title>
-        <script src="//cdn.ckeditor.com/4.11.4/standard/ckeditor.js"></script>
+        <!-- (text) ck editor library -->
+        <script src="//cdn.ckeditor.com/4.11.4/standard/ckeditor.js"></script>  
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
@@ -143,7 +144,7 @@ $result_cou = $stmt_cou->fetchAll(PDO::FETCH_ASSOC);
                     </form>  
                     <script>
                         CKEDITOR.replace( "content" );
-                </script>
+                </script>  
                </div>
             </div>
 
