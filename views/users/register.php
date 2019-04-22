8d23bcbc597e4b8eccfcf4e510bc53fd2f75b7f2
faithege <faithege@hotmail.co.uk>
2019-04-22 23:43:40 +0100

    Update register.php

--- views/users/register.php
+++ views/users/register.php
@@ -1,25 +1,22 @@
 
-<p>Please register below</p>
+<h4>Please register below</h4>
 
 <form method='POST' id="RegForm" name="RegForm" action="" onsubmit="return checkPassword()">
     <input type="text" id="Username" name="Username" autofocus="" placeholder="Username" value="" tabindex="1" required >
-        <?php if (isset($name_error)): ?>
-            <span><?php echo $name_error; ?></span>
-        <?php endif ?>
-                
     <input type="email" id="Email" name="Email" placeholder="Email" value="" tabindex="2" required >
     
              
     <input type="password" id="Password"  name="Password" placeholder="Password" value="" pattern="^[a-zA-Z]\w{3,14}$" tabindex="3" required >
-    <input type="password" id="ConfirmPassword" name="ConfirmPassword" id="password_confirmation" placeholder="Confirm Password" tabindex="4" required>
+    <input type="password" id="ConfirmPassword" name="ConfirmPassword" placeholder="Confirm Password" tabindex="4" required>
     <input type="submit" value="Go to login" tabindex="5"/>
-    <a href='?controller=user&action=login'> Already registered? <br> Click here to Login</a>
-    <!-- Comment-->
+    <br>
+    <br>
+    Already registered? <a href='?controller=user&action=login'> Click here to Login</a>
+    
 </form>
 
 <script>
-function checkPassword()
-{
+function checkPassword(){
     $password = document.forms["RegForm"]["Password"].value;     // to understand this syntax pls refer JS forms in W3schools website.
     $confirmation = document.forms["RegForm"]["ConfirmPassword"].value;
     if($password !== $confirmation)
@@ -32,6 +29,6 @@ function checkPassword()
         return true;
     }
      
-}
+}</script>
+<!--<script src="../jsFunctions.js" type="text/javascript"></script>-->
 
-</script>
