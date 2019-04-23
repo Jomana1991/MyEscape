248561643f838be648fba16471b08e4591cbb91d
faithege <faithege@hotmail.co.uk>
2019-04-23 23:53:15 +0100

    Update register.php

--- views/users/register.php
+++ views/users/register.php
@@ -1,14 +1,15 @@
 
-<h4>Please register below</h4>
+<h4>Fill in the below to update your credentials</h4>
 
-<form method='POST' id="RegForm" name="RegForm" action="" onsubmit="return checkPassword()">
-    <input type="text" id="Username" name="Username" autofocus="" placeholder="Username" value="" tabindex="1" required >
-    <input type="email" id="Email" name="Email" placeholder="Email" value="" tabindex="2" required >
+<form method='POST' id="regForm" name="regForm" action="" onsubmit="return checkPassword()">
+    <input type="text" id="username" name="username" autofocus="" placeholder="Username" value="" tabindex="1" required >
+    <input type="email" id="email" name="email" placeholder="Email" value="" tabindex="2" required >
     
              
-    <input type="password" id="Password"  name="Password" placeholder="Password" value="" pattern="^[a-zA-Z]\w{3,14}$" tabindex="3" required >
-    <input type="password" id="ConfirmPassword" name="ConfirmPassword" placeholder="Confirm Password" tabindex="4" required>
+    <input type="password" id="password"  name="password" placeholder="Password" value="" pattern="^[a-zA-Z]\w{3,14}$" tabindex="3" required >
+    <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" tabindex="4" required>
     <input type="submit" value="Go to login" tabindex="5"/>
+   
     <br>
     <br>
     Already registered? <a href='?controller=user&action=login'> Click here to Login</a>
@@ -17,8 +18,8 @@
 
 <script>
 function checkPassword(){
-    $password = document.forms["RegForm"]["Password"].value;     // to understand this syntax pls refer JS forms in W3schools website.
-    $confirmation = document.forms["RegForm"]["ConfirmPassword"].value;
+    $password = document.forms["regForm"]["password"].value;     // to understand this syntax pls refer JS forms in W3schools website.
+    $confirmation = document.forms["regForm"]["confirmPassword"].value;
     if($password !== $confirmation)
     {
         alert("Passwords do not match"); 
