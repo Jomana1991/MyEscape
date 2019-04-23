80e6a985b46d4214b59039b2472f82798d8df73f
vmashwini <ashwini.mandar@gmail.com>
2019-04-23 12:19:55 +0100

    Update blog_controller.php

--- controllers/blog_controller.php
+++ controllers/blog_controller.php
@@ -10,6 +10,7 @@ class BlogController {
     public function read() {
         // we expect a url of form ?controller=posts&action=show&id=x
         // without an id we just redirect to the error page as we need the post id to find it in the database
+         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         if (!isset($_GET['blogID']))
             return call('pages', 'error');
 
@@ -20,6 +21,35 @@ class BlogController {
         } catch (Exception $ex) {
             return call('pages', 'error');
         }
+         }
+        else {
+             $blogid = $_GET['blogID'];
+            Blog::addComment($blogid);
+
+         $blog = Blog::find($blogid);
+
+           require_once('views/blogs/read.php');
+            
+        }
+    }
+
+    public function addComment() {
+         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            if (!isset($_GET['blogID']))
+                return call('pages', 'error');#requires better exception handling
+            // we use the given id to get the correct blog for updating
+            $blog = Blog::find($_GET['blogID']);
+
+     require_once('views/blogs/find.php');
+        }
+        else {
+            $blogid = $_GET['blogID'];
+            Blog::addComment($blogid);
+
+         $blog = Blog::find($blogid);
+
+           require_once('views/blogs/find.php');
+        }
     }
 
     public function create() {
@@ -28,13 +58,12 @@ class BlogController {
         // else it's a POST so add to the database and redirect to readAll action
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             require_once('views/blogs/create.php');
-            
         } else {
             Blog::add();
 
             #Can't get this to work
             #$blogs = User::readMine($_GET['username']);
-            require_once('./models/user.php'); 
+            require_once('./models/user.php');
 
             $blogs = User::readMine($_SESSION['username']);
             require_once('views/users/readMine.php');
@@ -63,8 +92,9 @@ class BlogController {
     public function delete() {
         Blog::delete($_GET['blogID']);
 
-        $blogs = Blog::all();
-        require_once('views/blogs/readAll.php');
+        require_once('./models/user.php');
+        $blogs = User::readMine($_SESSION['username']);
+        require_once('views/users/readMine.php');
     }
 
     public function search() {
@@ -76,7 +106,6 @@ class BlogController {
                 Try {
                     $blogs = Blog::search();
                     require_once('views/blogs/search.php');
-                    
                 } catch (Exception $ex) {
                     return call('pages', 'error');
                 }
@@ -85,37 +114,38 @@ class BlogController {
             }
         }
     }
-    
-    
+
     public function likeBlog() {
-        
+
         if (!isset($_GET['blogID']))
             return call('pages', 'error');
 
         try {
             Blog::like($_GET['blogID']);
             $blog = Blog::find($_GET['blogID']);
-            require_once('views/blogs/read.php');#change to ajax?
-            
+            require_once('views/blogs/read.php'); #change to ajax?
         } catch (Exception $ex) {
             return call('pages', 'error');
-        }       
+        }
     }
-    
-    
+
     public function dislikeBlog() {
-       if (!isset($_GET['blogID']))
+        if (!isset($_GET['blogID']))
             return call('pages', 'error');
 
         try {
             Blog::dislike($_GET['blogID']);
             $blog = Blog::find($_GET['blogID']);
-            require_once('views/blogs/read.php');#change to ajax?
-            
+            require_once('views/blogs/read.php'); #change to ajax?
         } catch (Exception $ex) {
             return call('pages', 'error');
-        }   
+        }
     }
+<<<<<<< HEAD
+=======
+
+}
+>>>>>>> master
 
 public function viewBlog() {
         
