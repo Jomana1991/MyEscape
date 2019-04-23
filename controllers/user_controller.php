9299e24b41f3f81ab08d076568d85798ccfed442
faithege <faithege@hotmail.co.uk>
2019-04-23 23:53:09 +0100

    Update user_controller.php

--- controllers/user_controller.php
+++ controllers/user_controller.php
@@ -43,7 +43,7 @@ class userController {
             User::register();
              
 //            $blogs = User::login(); 
-//            require_once('views/blogs/login.php');
+//            require_once('views/users/login.php');
       }
       
     }
@@ -64,25 +64,32 @@ class userController {
     }
     
     
+    
+    
     public function changePassword() {
-//        if($_SERVER['REQUEST_METHOD'] == 'GET'){
-//             
-//            if (!empty($_SESSION['username'])) {
-//            // if logged in can skip login page and go straight to create blog page
-//            header('location:?controller=blog&action=create');
-//            }        
-//            else{
-//                 require_once('views/users/login.php');
-//            }
-//        } 
-//         else { 
-//            User::login();
-//             
-//            require_once('views/blogs/create.php');
-//            require_once('./models/blog.php');
-//            $blogs = Blog::add();
-//           
- //     }
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            
+            require_once('views/users/changePassword.php');
+        }       
+            else{
+
+                if (User::confirmUserExists()){
+                    try{ User::modify();
+
+                    require_once('views/users/login.php');
+                    }
+                    catch (Exception $ex){
+                    return call('pages','error');
+                    }
+                }
+                
+                else{
+                    $message = "Username does not exist.";
+        
+                    echo '<script type="text/javascript">alert("'.$message.'");history.go(-1);</script>';
+                }
+        }
+      
     }
     
  }
