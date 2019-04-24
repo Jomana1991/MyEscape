59462a837be6f91083a2a0c65c05260315a5b52a
vmashwini <ashwini.mandar@gmail.com>
2019-04-24 18:39:12 +0100

    Update register.php

--- views/users/register.php
+++ views/users/register.php
@@ -28,21 +28,6 @@
     <a href='?controller=user&action=login'> Existing user login? <br> Click here to Login</a>
     <!-- Comment-->
 
-<h4>Fill in the below to update your credentials</h4>
-
-<form method='POST' id="regForm" name="regForm" action="" onsubmit="return checkPassword()">
-    <input type="text" id="username" name="username" autofocus="" placeholder="Username" value="" tabindex="1" required >
-    <input type="email" id="email" name="email" placeholder="Email" value="" tabindex="2" required >
-    
-             
-    <input type="password" id="password"  name="password" placeholder="Password" value="" pattern="^[a-zA-Z]\w{3,14}$" tabindex="3" required >
-    <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" tabindex="4" required>
-    <input type="submit" value="Go to login" tabindex="5"/>
-   
-    <br>
-    <br>
-    Already registered? <a href='?controller=user&action=login'> Click here to Login</a>
-    
 
 </form>
 <div id="message">
