addf651a5144d1530707d71dcca89ae099915883
faithege <faithege@hotmail.co.uk>
2019-04-25 19:29:44 +0100

    Update search.php

--- views/blogs/search.php
+++ views/blogs/search.php
@@ -13,9 +13,9 @@
 <html>
     <head>
         <title> </title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        
+<!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
     </head>
     <body>
 <p>Search for a blog below</p>
@@ -96,6 +96,7 @@ $(document).ready(function(){
 
 </script>-->
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     </body>
 </html>
 
