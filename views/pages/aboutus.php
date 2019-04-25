70977dc79db1b4850282dfcc9856470c2ce2b9a5
faithege <faithege@hotmail.co.uk>
2019-04-25 21:22:42 +0100

    Update aboutus.php

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -1,12 +1,39 @@
 <!--<link href="css/styles-general.css" rel="stylesheet" type="text/css"/>-->
   
-    <h2>MyEscape</h2>
-    <h3>Hey.. we are ADFJ</h3> 
-    <p> We are 'My Escape', a global travel community created to inspire and connect travellers around the world.
-        we are obsessed with exploring the world, meeting new people and getting as lost as possible.
-        I hope this site gives you some inspiration, handy tips to go and chase your own adventures.
-        Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!
+    <h2>About MyEscape</h2>
+    
+    <h3>Welcome!</h3> 
+    <p>
+        We are 'MyEscape', a global travel community created to inspire and connect travellers around the world.
+        We are obsessed with exploring the world, meeting new people and getting as lost as possible.
+        <br>
+        We hope this website gives you some inspiration, handy tips to go and chase your own adventures, and a place for you to report back to fellow travellers.
+        <br>
+        <span style="color: #395C6B">Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!</span>
     </p>
+    <h3>What can I do on this website?</h3> 
+    <p> 
+        <span class="glyphicon glyphicon-globe"></span> Browse the website for inspiration, advice, and memories
+        <br><span class="glyphicon glyphicon-globe"></span> Find particular blogs by geography, category, or keywords
+        <br><span class="glyphicon glyphicon-globe"></span> Register to become part of the ever-growing MyEscape community
+        <br><span class="glyphicon glyphicon-globe"></span> Share your own content and inspire even more people
+        <br><span class="glyphicon glyphicon-globe"></span> Incorporate your Instagram and Twitter feeds with your blog posts
+    </p>
+    <h3>Meet the team</h3> 
+    <img src="../images/ash.jpg" class="avatar" alt="Ash" style="width:200px">
+    <img src="../images/dhanu.jpg" class="avatar" alt="Dhanu" style="width:200px">
+    <img src="../images/faith.jpg" class="avatar" alt="Faith" style="width:200px">
+    <img src="../images/jomana.jpg" class="avatar" alt="Jomana" style="width:200px">
+        circle images - click on and see bio some how? country food or short bio
+        responsive
+        <img src="../images/ash.jpg" alt=""/>
+    <h3>Stay in touch</h3> 
+    <p> dummy socialmedia links
+        mini navbar at top which takes to lower pages in site?
+    </p>    
+      
+        
+    
     
  
  
