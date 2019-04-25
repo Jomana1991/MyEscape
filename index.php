550791b4a7ed2a6694ca7a9fc487ab36a72a331e
faithege <faithege@hotmail.co.uk>
2019-04-25 18:31:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,38 +6,26 @@
     </head>
     <body>
         <?php
-    require_once('connection.php');
-        
-    if (isset($_GET['controller']) && isset($_GET['action'])) {
-        $controller = $_GET['controller'];
-        $action     = $_GET['action'];
-  } else {
-        $controller = 'pages';
-        $action     = 'home';
-  }
-    
-  #require_once('views/layout.php')
-  
-    if (isset($_GET['action']) && ($_GET['action'] == ('likeBlog' || 'dislikeBlog'))) {
-          require_once('routes.php'); 
-    }
-    else {
-    require_once('views/layout.php');
-    }
-  
-  
-//   if (isset($_GET['action']) && ($_GET['action'] = ('likeBlog' || 'dislikeBlog'))) {
-//        require_once('routes.php');
-//  }
-//  elseif($_GET['controller']='pages'){
-//      require_once('views/layout.php');   
-//  }
-//    else {
-//        require_once('views/layout.php');      
-//  }
- 
-  #;
-    
-        ?>
+            require_once('connection.php');
+
+            if (isset($_GET['controller']) && isset($_GET['action'])) {
+                $controller = $_GET['controller'];
+                $action     = $_GET['action'];
+            } else {
+                $controller = 'pages';
+                $action     = 'home';
+            }
+
+          #require_once('views/layout.php')
+
+            if ((isset($_GET['action'])) && (($_GET['action'] == ('likeBlog' || 'dislikeBlog')))) {
+                  require_once('routes.php'); 
+            }
+            else {
+            require_once('views/layout.php');
+            }
+
+
+                ?>
     </body>
 </html>
