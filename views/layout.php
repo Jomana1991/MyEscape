0dbb0a6dc045c4d63f3a86557b4f07f051fc633a
faithege <faithege@hotmail.co.uk>
2019-04-25 19:41:03 +0100

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -3,7 +3,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <!--        <link href="css/styles-general.css" rel="stylesheet" type="text/css"/>-->
-<!--<style>
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Oswald|Roboto');
     body {
   background-color: #F6F4D2;
 }
@@ -14,43 +15,45 @@
 
 p {
   color: #001D4A;
-  font-family: 14px 'Roboto', sans-serif;
+  font: 14px 'Roboto', sans-serif;
 }
-</style>-->
+</style>
     
     <title>MyEscape</title>
   </head>
   <body>
-    <header class="w3-container w3-gray">
-      <a href='?controller=pages&action=home'>Home</a>
-      <a href='?controller=pages&action=aboutus'>About Us</a>  
-      <a href='?controller=blog&action=readAll'>Blogs</a>
-      <a href='?controller=blog&action=search'>Search</a>
-      <?php
-      session_start();
-      if (empty($_SESSION['username'])){
-        echo "<a href='?controller=user&action=login'>Login</a> ";
-        echo "<a href='?controller=user&action=register'>Register</a> ";
-      }
-      else{
-        echo "<a href='?controller=blog&action=create'>Profile Page</a> ";
-        echo "<a href='?controller=pages&action=logout'>Logout</a> ";
-      }
-      ?>
-      <a href='?controller=user&action=contactus'>Contact Us</a>
-      
+      <div class="container-fluid">  
+        <header class="w3-container w3-gray">
+          <a href='?controller=pages&action=home'>Home</a>
+          <a href='?controller=pages&action=aboutus'>About Us</a>  
+          <a href='?controller=blog&action=readAll'>Blogs</a>
+          <a href='?controller=blog&action=search'>Search</a>
+          <?php
+          session_start();
+          if (empty($_SESSION['username'])){
+            echo "<a href='?controller=user&action=login'>Login</a> ";
+            echo "<a href='?controller=user&action=register'>Register</a> ";
+          }
+          else{
+            echo "<a href='?controller=blog&action=create'>Profile Page</a> ";
+            echo "<a href='?controller=pages&action=logout'>Logout</a> ";
+          }
+          ?>
+          <a href='?controller=user&action=contactus'>Contact Us</a>
 
-    </header>
-      
-<div class="w3-container w3-pink">
-    <?php require_once('routes.php'); ?>
-   
-</div>
-<div class="w3-container w3-gray">
-    <footer >
-        Copyright &COPY; <?= date('Y'); ?>
-    </footer>
-</div>
+
+        </header>
+
+    <div class="w3-container w3-pink">
+        <?php require_once('routes.php'); ?>
+
+    </div>
+    <div class="w3-container w3-gray">
+        <footer >
+            Copyright &COPY; <?= date('Y'); ?>
+        </footer>
+    </div>
+      </div>
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
