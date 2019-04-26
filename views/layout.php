3d637d3bb783d42153c80c3e4f324e0c7d71d81d
faithege <faithege@hotmail.co.uk>
2019-04-26 20:12:18 +0100

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -4,18 +4,24 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <!--        <link href="css/styles-general.css" rel="stylesheet" type="text/css"/>-->
 <style>
-    @import url('https://fonts.googleapis.com/css?family=Cinzel+Decorative|Sacramento|Oswald|Roboto');
+    @import url('https://fonts.googleapis.com/css?family=Cinzel+Decorative|Sacramento|Oswald|Roboto|Courgette');
  body {
   background-color: #F6F4D2;
 }
 
 h2 {
   color: #395C6B;
-  font: 72px 'Sacramento', cursive;
-/*  font-weight: bold;*/
+  font: 72px 'Roboto condensed', cursive;
+/*  font-weight: bold;
 }
 
 h3 {
+  color: #001D4A;
+  font-family: 'Roboto', sans-serif;
+  font-weight: bold;
+}
+
+h4 {
   color: #001D4A;
   font-family: 'Roboto', sans-serif;
   font-weight: bold;
@@ -26,8 +32,52 @@ p {
   font: 16px 'Roboto', sans-serif;
 }
 
-.avatar {
+/*.avatar {
   border-radius: 50%;
+}*/
+
+.flip-card {
+  background-color: transparent;
+  width: 200px;
+  height: 200px;
+/*  border-radius: 50%;*/
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: #FFFFFF;
+  color: white;
+  transform: rotateY(180deg);
 }
 </style>
     
