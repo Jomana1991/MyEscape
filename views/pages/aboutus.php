44309a8be096a0e60b6157dea2a4eb33974248cd
faithege <faithege@hotmail.co.uk>
2019-04-26 20:12:26 +0100

    Update aboutus.php

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -11,6 +11,7 @@
         <br>
         <span style="color: #395C6B">Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!</span>
     </p>
+    <br>
     <h3>What can I do on this website?</h3> 
     <p> 
         <span class="glyphicon glyphicon-globe"></span> Browse the website for inspiration, advice, and memories
@@ -19,14 +20,28 @@
         <br><span class="glyphicon glyphicon-globe"></span> Share your own content and inspire even more people
         <br><span class="glyphicon glyphicon-globe"></span> Incorporate your Instagram and Twitter feeds with your blog posts
     </p>
+    <br>
     <h3>Meet the team</h3> 
-    <img src="../images/ash.jpg" class="avatar" alt="Ash" style="width:200px">
-    <img src="../images/dhanu.jpg" class="avatar" alt="Dhanu" style="width:200px">
-    <img src="../images/faith.jpg" class="avatar" alt="Faith" style="width:200px">
-    <img src="../images/jomana.jpg" class="avatar" alt="Jomana" style="width:200px">
+    <img src="/MyEscape/views/images/ash.jpg" class="avatar" alt="Ash" style="width:200px">     
+    <img src="/MyEscape/views/images/dhanu.jpg" class="avatar" alt="Dhanu" style="width:200px">     
+    
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src="/MyEscape/views/images/faith.jpg" class="avatar" alt="Faith" style="width:200px">
+            </div>
+            <div class="flip-card-back">
+                <h4>Faith</h4> 
+                <p>Top 3 places: Tanzania, Cuba, Brazil</p>
+                <p>Favourite cuisine: Mexican</p> 
+                <p>Travel tip: Never turn down a clean toilet opportunity!</p>
+            </div>
+        </div>
+    </div>
+    <img src="/MyEscape/views/images/jomana.jpg" class="avatar" alt="Jomana" style="width:200px">
         circle images - click on and see bio some how? country food or short bio
         responsive
-        <img src="../images/ash.jpg" alt=""/>
+    <br>
     <h3>Stay in touch</h3> 
     <p> dummy socialmedia links
         mini navbar at top which takes to lower pages in site?
