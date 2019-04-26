0a918a9e64e5b6ff8ae05fca0a14d736e79cb5b3
faithege <faithege@hotmail.co.uk>
2019-04-26 22:06:59 +0100

    Update aboutus.php

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -1,51 +1,115 @@
 <!--<link href="css/styles-general.css" rel="stylesheet" type="text/css"/>-->
-  
-    <h2>About MyEscape</h2>
-    
-    <h3>Welcome!</h3> 
-    <p>
-        We are 'MyEscape', a global travel community created to inspire and connect travellers around the world.
-        We are obsessed with exploring the world, meeting new people and getting as lost as possible.
+  <div class="row">
+    <div class="col-xs-2"  >
+    </div>
+    <div class="col-xs-8"  >
+        <h2>About MyEscape</h2>
+    </div>
+    <div class="col-xs-2"  >
+    </div>
+  </div>
+  <div class="row">
+     <div class="col-xs-2"  >
+     </div>
+    <div class="col-xs-8"  >
+        <h3>Welcome!</h3> 
+        <p>
+            We are 'MyEscape', a global travel community created to inspire and connect travellers around the world.
+            We are obsessed with exploring the world, meeting new people and getting as lost as possible.
+            <br>
+            We hope this website gives you some inspiration, handy tips to go and chase your own adventures, and a place for you to report back to fellow travellers.
+            <br>
+            <span style="color: #395C6B">Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!</span>
+        </p>
         <br>
-        We hope this website gives you some inspiration, handy tips to go and chase your own adventures, and a place for you to report back to fellow travellers.
+        <h3>What can I do on this website?</h3> 
+        <p> 
+            <span class="glyphicon glyphicon-globe"></span> Browse the website for inspiration, advice, and memories
+            <br><span class="glyphicon glyphicon-globe"></span> Find particular blogs by geography, category, or keywords
+            <br><span class="glyphicon glyphicon-globe"></span> Register to become part of the ever-growing MyEscape community
+            <br><span class="glyphicon glyphicon-globe"></span> Share your own content and inspire even more people
+            <br><span class="glyphicon glyphicon-globe"></span> Incorporate your Instagram and Twitter feeds with your blog posts
+        </p>
         <br>
-        <span style="color: #395C6B">Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!</span>
-    </p>
-    <br>
-    <h3>What can I do on this website?</h3> 
-    <p> 
-        <span class="glyphicon glyphicon-globe"></span> Browse the website for inspiration, advice, and memories
-        <br><span class="glyphicon glyphicon-globe"></span> Find particular blogs by geography, category, or keywords
-        <br><span class="glyphicon glyphicon-globe"></span> Register to become part of the ever-growing MyEscape community
-        <br><span class="glyphicon glyphicon-globe"></span> Share your own content and inspire even more people
-        <br><span class="glyphicon glyphicon-globe"></span> Incorporate your Instagram and Twitter feeds with your blog posts
-    </p>
-    <br>
-    <h3>Meet the team</h3> 
-    <img src="/MyEscape/views/images/ash.jpg" class="avatar" alt="Ash" style="width:200px">     
-    <img src="/MyEscape/views/images/dhanu.jpg" class="avatar" alt="Dhanu" style="width:200px">     
-    
-    <div class="flip-card">
-        <div class="flip-card-inner">
-            <div class="flip-card-front">
-                <img src="/MyEscape/views/images/faith.jpg" class="avatar" alt="Faith" style="width:200px">
-            </div>
-            <div class="flip-card-back">
-                <h4>Faith</h4> 
-                <p>Top 3 places: Tanzania, Cuba, Brazil</p>
-                <p>Favourite cuisine: Mexican</p> 
-                <p>Travel tip: Never turn down a clean toilet opportunity!</p>
+        <h3>Meet the team</h3>
+        <div class="col-xs-2"  >
+        </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-2"  >
+    </div>
+    <div class="col-md-2"  >
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/MyEscape/views/images/ash.jpg" class="avatar" alt="Ash">  
+                </div>
+                <div class="flip-card-back">
+                    <h4>Ash</h4> 
+                    <p>Top 3 places: x,y,z</p>
+                    <p>Favourite cuisine: abc</p> 
+                    <p>Travel tip: Insert text here</p>
+                </div>
             </div>
         </div>
     </div>
-    <img src="/MyEscape/views/images/jomana.jpg" class="avatar" alt="Jomana" style="width:200px">
-        circle images - click on and see bio some how? country food or short bio
-        responsive
+    <div class="col-md-2"  >
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/MyEscape/views/images/dhanu.jpg" class="avatar" alt="Dhanu"> 
+                </div>
+                <div class="flip-card-back">
+                    <h4>Dhanu</h4> 
+                    <p>Top 3 places: x,y,z</p>
+                    <p>Favourite cuisine: abc</p> 
+                    <p>Travel tip: Insert text here</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"  >
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/MyEscape/views/images/faith.jpg" class="avatar" alt="Faith">
+                </div>
+                <div class="flip-card-back">
+                    <h4>Faith</h4> 
+                    <p>Top 3 places: Tanzania, Cuba, Brazil</p>
+                    <p>Favourite cuisine: Mexican</p> 
+                    <p>Travel tip: Never turn down a clean toilet<br>opportunity!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-2"  >
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="/MyEscape/views/images/jomana.jpg" class="avatar" alt="Jomana">
+                </div>
+                <div class="flip-card-back">
+                    <h4>Jomana</h4> 
+                    <p>Top 3 places: x,y,z</p>
+                    <p>Favourite cuisine: abc</p> 
+                    <p>Travel tip: Insert text here</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-2"  >
+    </div>
+  </div>
+    
+  <div class="row">       
     <br>
     <h3>Stay in touch</h3> 
     <p> dummy socialmedia links
         mini navbar at top which takes to lower pages in site?
-    </p>    
+    </p>
+  </div>
       
         
     
