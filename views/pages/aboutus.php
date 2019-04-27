36483add403b36c95c51e15b5f751d7641a2c3ee
vmashwini <ashwini.mandar@gmail.com>
2019-04-27 21:04:20 +0100

    Update aboutus.php

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -1,12 +1,43 @@
 <!--<link href="css/styles-general.css" rel="stylesheet" type="text/css"/>-->
-  
-    <h2>MyEscape</h2>
-    <h3>Hey.. we are ADFJ</h3> 
-    <p> We are 'My Escape', a global travel community created to inspire and connect travellers around the world.
-        we are obsessed with exploring the world, meeting new people and getting as lost as possible.
-        I hope this site gives you some inspiration, handy tips to go and chase your own adventures.
-        Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!
-    </p>
-    
- 
- 
+
+<html>
+    <head>
+        <title>about us</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        
+        
+    </head>
+
+    <body>
+
+        <div class="main">
+            <div class="container">
+                <div class="row">
+                    <div class=" text-center col-md-10 col-md-offset-2">
+
+                        <div id="orangefloat" > 
+                            <h2>MyEscape</h2>
+                            <h3>Hey.. we are ADFJ</h3> 
+                            <p> We are 'My Escape', a global travel community created to inspire and connect travellers around the world.
+                                we are obsessed with exploring the world, meeting new people and getting as lost as possible.
+                                I hope this site gives you some inspiration, handy tips to go and chase your own adventures.
+                                Travel is the best kind of education so go out and discover yourself, even if it’s just outside your own doorstep!
+                            </p>
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
+
