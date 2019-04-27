00792cdd4c7ffb256172267713a120c466ced641
vmashwini <ashwini.mandar@gmail.com>
2019-04-27 10:45:39 +0100

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -105,9 +105,9 @@ p {
     <?php require_once('footer.html'); ?>
       </div>
   
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-<!--  <script src="jsFunctions.js" type="text/javascript"></script>-->
+ <script src="jsFunctions.js" type="text/javascript"></script>-->
   
   </body>
 </html>
