bda25a90f7f65beb703606f9fd1c4275ef1ff537
faithege <faithege@hotmail.co.uk>
2019-04-27 22:51:34 +0100

    Update aboutus.php

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -3,9 +3,7 @@
 <html>
     <head>
         <title>About us</title>
-<!--        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-<!--        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />-->
+
         
         <style>
 h2 {
@@ -221,7 +219,7 @@ h4 {
             </div>
 
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
     </body>
 </html>
