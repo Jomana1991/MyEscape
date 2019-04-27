f8d137dbdfae0f0ed81f50257fd7b27539f729f7
vmashwini <ashwini.mandar@gmail.com>
2019-04-27 19:58:36 +0100

    Update search.php

--- views/blogs/search.php
+++ views/blogs/search.php
@@ -12,7 +12,7 @@
     <head>
         <title> </title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <style>
             @import url('https://fonts.googleapis.com/css?family=Oswald');
