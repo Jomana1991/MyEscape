1ca03dd24251a16876d554794d4acefdb4e4dd65
vmashwini <ashwini.mandar@gmail.com>
2019-04-27 21:04:11 +0100

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 
 <html>
+
     <head>
         <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -17,14 +18,65 @@
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/bootstrap-responsive.css">
         <meta charset="UTF-8">
+
+
 <style>
     @import url('https://fonts.googleapis.com/css?family=Oswald|Roboto');
-    body {
-  background-color: #F6F4D2;
+    
+    
+.main{
+
+    display: table;
+    height: 100%;
+    position: relative;
+    width: 100%;
+    background-size: cover;
+
+
+
 }
+.main:before {
+    content: '';
+    position: absolute; 
+    opacity: 0.4;
+    background: url(./img/share.jpg) no-repeat 0 50%; 
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    background-repeat: no-repeat;
+
+
+}
+
+#orangefloat {
+
+    height: auto;               
+
+    background: linear-gradient(to right, #EABE7C, #E88D67);
+/*    font-size: 45px;
+    font-family: 'Oswald', sans-serif;*/
+
+    color: #fff;
+
+    min-height: 500px;
+    opacity:0.8;
+
+
+}
+
+.container {
+    margin-left: 10px;
+    margin-top: 50px;
+}
+/*    body {
+
+  background-color: #F6F4D2;
+}*/
+
 /*h1 {
   color: blue;
 }*/
+<<<<<<< HEAD
     p {
       color: #001D4A;
       font: 14px 'Roboto', sans-serif;
@@ -34,6 +86,14 @@
        background-color: #E88D67; 
        opacity: 0.75;
     }
+=======
+
+/*p {
+  color: #001D4A;
+  font: 14px 'Roboto', sans-serif;
+}*/
+
+>>>>>>> e8a1342cff298991c3ea84fbd761cc10d8bf323d
 </style>
     
   </head>
