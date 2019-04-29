afcabe32a4d7131d97741f0f185451994c500e4f
faithege <faithege@hotmail.co.uk>
2019-04-29 23:29:52 +0100

    Update user.php

--- models/user.php
+++ models/user.php
@@ -209,7 +209,7 @@ class User {
         try{
         $req = $db->prepare("Call updatePassword(:username, :newPassword);");
         $req->bindParam(':username', $username);
-        $req->bindParam(':newPassword', $newPasswordHashed);
+        $req->bindParam(':newPassword', $newPassword);
         
         $passwordUpdateDetails = filter_input_array(INPUT_POST);
         //asking whether title is empty refers to whether the addform has been submitted yet, if not the query is run
@@ -218,7 +218,7 @@ class User {
                 ${$formDetail} = User::filterInput($formDetail);
             }
             
-            $newPasswordHashed=password_hash($newPassword, PASSWORD_DEFAULT);
+            #$newPasswordHashed=password_hash($newPassword, PASSWORD_DEFAULT);
             
             $req->execute();
             
